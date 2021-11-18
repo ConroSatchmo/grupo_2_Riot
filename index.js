3ba033926fcb9fd0ddc019549cf6bdf9b38fafba
jmanuelpereyra <jmanuelpereyra@gmail.com>
2021-11-18 18:25:20 -0300

    hice el login y algunos efectos

--- index.js
+++ index.js
@@ -13,9 +13,25 @@ app.use(require("./routes/index"));
 
 // Static
 app.use(express.static(publicPath));
+// app.use(express.json);
+// app.use(urlencoded({ extended: true }));
 
 module.exports = app;
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+// app.post("/register", (req, res) => {
+//   console.log(req.body);
+//   res.sendFile(path.resolve(__dirname, "./views/register.html"));
+// });
+// app.post("/login", (req, res) => {
+//   console.log(req.body);
+//   res.sendFile(path.resolve(__dirname, "./views/login.html"));
+// });
