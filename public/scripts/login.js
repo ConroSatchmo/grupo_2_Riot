0d93e1ec7a7805022506ad8ec0a4226eac83cce5
ConroSatchmo <conradonegrin@gmail.com>
2021-11-22 13:14:46 -0300

    sin prductborder

---
+++ public/scripts/login.js
@@ -0,0 +1,17 @@
+function validateForm() {
+  const userName = document.forms["myForm"]["userName"].value;
+  const userPassword = document.forms["myForm"]["userPassword"].value;
+  if (userName == "") {
+    alert("Debes completar el nombre");
+    return false;
+  }
+  if (userPassword == "") {
+    alert("Debes completar la contraseña");
+    return false;
+  }
+
+  if (userPassword.length > 0 && userPassword.length < 6) {
+    alert("La contraseña debe tener al menos 6 caracteres");
+    return false;
+  }
+}
