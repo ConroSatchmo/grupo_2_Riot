e7d0df7638035703019d6decd2bf0d79a1c59355
ConroSatchmo <conradonegrin@gmail.com>
2022-01-04 11:51:25 -0300

    con cambios de createproduct

--- src/controllers/createProduct.js
+++ src/controllers/createProduct.js
@@ -1,6 +1,23 @@
 const createProductController = {
-  renderCreateProduct: (req, res) => {
+  render: (req, res) => {
     res.render("createProduct");
+  },
+  store: (req, res) => {
+    const product = {
+      id: req.body.Id,
+      name: req.body.nombreDeProducto,
+      description: req.body.descripcion,
+      color: req.body.color,
+      brand: req.body.marca,
+      price: req.body.precio,
+      images: [
+        req.body.imagenDelProducto1,
+        req.body.imagenDelProducto2,
+        req.body.imagenDelProducto3,
+        req.body.imagenDelProducto4,
+      ],
+    };
+    res.redirect("/products");
   },
 };
 
--- src/routes/createProduct.js
+++ src/routes/createProduct.js
@@ -3,6 +3,7 @@ const router = Router();
 
 const createProductController = require("../controllers/createProduct");
 
-router.get("/", createProductController.renderCreateProduct);
+router.get("/", createProductController.render);
+router.post("/", createProductController.store);
 
 module.exports = router;
