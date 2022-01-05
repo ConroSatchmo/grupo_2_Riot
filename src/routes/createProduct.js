28b9804571a27fbaa777424eece25a74ef6444d0
ConroSatchmo <conradonegrin@gmail.com>
2022-01-05 19:34:52 -0300

    con create

--- src/routes/createProduct.js
+++ src/routes/createProduct.js
@@ -3,26 +3,9 @@ const router = Router();
 const multer = require("multer");
 const createProductController = require("../controllers/createProduct");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../public/images/products"));
-  },
-  filename: (req, file, cb) => {
-    const newFilename =
-      "product-" + Date.now() + path.extname(file.originalname);
-    cb(null, newFilename);
-  },
-});
-const upload = multer({ storage });
-
-/*** CREATE ***/
-router.post("/create", createProductController.store); //, upload.array("imagen")
-
-/*** EDIT ***/
-router.get("/edit/:id", createProductController.edit);
-router.put("/edit/:id", upload.array("imagen"), createProductController.update);
-
-/*** DELETE ***/
-router.delete("/:id", createProductController.delete);
+router.get("/", createProductController.render);
+router.post("/", createProductController.store);
+router.put("/", createProductController.edit);
+router.delete("/", createProductController.delete);
 
 module.exports = router;
--- src/routes/products.js
+++ src/routes/products.js
@@ -6,5 +6,12 @@ const productsController = require("../controllers/products");
 router.get("/", productsController.renderProducts);
 router.get("/create", productsController.renderProductCreate);
 router.get("/:id", productsController.renderProductDetail);
+// router.get("/:id/edit", productsController.renderProductEdit);
+
+// router.post("/", productsController.createProduct);
+
+// router.put("/:id", productsController.updateProduct);
+
+// router.delete("/:id", productsController.deleteProduct);
 
 module.exports = router;
