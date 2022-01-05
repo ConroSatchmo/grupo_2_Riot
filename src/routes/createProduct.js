a4c0c272c2021f63ae84beef812726dcafa03214
jmanuelpereyra <jmanuelpereyra@gmail.com>
2022-01-05 20:02:09 -0300

    ahora si

--- src/routes/createProduct.js
+++ src/routes/createProduct.js
@@ -4,9 +4,27 @@ const router = Router();
 const multer = require("multer");
 const createProductController = require("../controllers/createProduct");
 
-router.get("/", createProductController.render);
-router.post("/", createProductController.store);
-router.put("/", createProductController.edit);
-router.delete("/", createProductController.delete);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../public/images/products"));
+  },
+  filename: (req, file, cb) => {
+    const newFilename =
+      "product-" + Date.now() + path.extname(file.originalname);
+    cb(null, newFilename);
+  },
+});
+
+const upload = multer({ storage });
+
+/*** CREATE ***/
+router.post("/", upload.array("imagen"), createProductController.store);
+
+/*** EDIT ***/
+router.get("/edit/:id", createProductController.edit);
+router.put("/edit/:id", upload.array("imagen"), createProductController.update);
+
+/*** DELETE ***/
+router.delete("/:id", createProductController.delete);
 
 module.exports = router;
