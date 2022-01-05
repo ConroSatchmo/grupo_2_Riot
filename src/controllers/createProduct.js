910487ba3985fba252bf89411018c6b0f87cf2ad
jmanuelpereyra <jmanuelpereyra@gmail.com>
2022-01-05 18:25:37 -0300

    intento de CRUD

--- src/controllers/createProduct.js
+++ src/controllers/createProduct.js
@@ -1,30 +1,72 @@
+const fs = require("fs");
 const path = require("path");
 
+const productsFilePath = path.join(__dirname, "../database/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const createProductController = {
-  render: (req, res) => {
-    res.render("createProduct");
-  },
   store: (req, res) => {
     const product = {
-      id: req.body.Id,
+      id: req.body.id,
       name: req.body.nombreDeProducto,
       description: req.body.descripcion,
       color: req.body.color,
       brand: req.body.marca,
       price: req.body.precio,
-      images: [
-        req.body.imagenDelProducto1,
-        req.body.imagenDelProducto2,
-        req.body.imagenDelProducto3,
-        req.body.imagenDelProducto4,
-      ],
+      images: req.body.imagen,
     };
+    products.unshift(product);
+    const productJSON = JSON.stringify(products);
+    fs.writeFileSync(productsFilePath, productJSON);
     res.redirect("/products");
   },
-  edit: (req, res) => {},
+  edit: (req, res) => {
+    const id = req.params.id;
+    const productToEdit = products.filter((product) => product.id === id);
+    res.render("editProduct", { product: productToEdit[0] });
+  },
+  update: (req, res) => {
+    const id = req.params.id;
+    let image;
+    const { name, description, color, brand, price, images } = req.body;
+    products.forEach((product) => {
+      if (product.id === id) {
+        if (req.file) {
+          image = req.file.filename;
+          fs.unlink(imagesPath + product.images, (err) => {
+            if (err) throw err;
+            console.log(err);
+          });
+        }
 
-  delete: (req, res) => {},
+        product.name = name;
+        product.description = description;
+        product.color = color;
+        product.brand = brand;
+        product.price = Number(price);
+        product.images = images || product.images;
+
+        const productJSON = JSON.stringify(products);
+        fs.writeFileSync(productsFilePath, productJSON);
+      }
+    });
+    res.redirect("/products");
+  },
+  delete: (req, res) => {
+    const id = req.params.id;
+    const updateProducts = products.filter((product) => product.id != id);
+    const productJSON = JSON.stringify(updateProducts);
+    fs.writeFileSync(productsFilePath, productJSON);
+    products.forEach((product) => {
+      if (product.id === id) {
+        fs.unlink(imagesPath + product.image, (err) => {
+          if (err) throw err;
+          console.log(err);
+        });
+      }
+    });
+    res.redirect("/products");
+  },
 };
 
 module.exports = createProductController;
--- src/routes/products.js
+++ src/routes/products.js
@@ -6,12 +6,5 @@ const productsController = require("../controllers/products");
 router.get("/", productsController.renderProducts);
 router.get("/create", productsController.renderProductCreate);
 router.get("/:id", productsController.renderProductDetail);
-// router.get("/:id/edit", productsController.renderProductEdit);
-
-// router.post("/", productsController.createProduct);
-
-// router.put("/:id", productsController.updateProduct);
-
-// router.delete("/:id", productsController.deleteProduct);
 
 module.exports = router;
