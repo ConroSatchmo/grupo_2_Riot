c75592a7f25e8ee0a47d004659342ade7cdafea6
jmanuelpereyra <jmanuelpereyra@gmail.com>
2022-01-16 19:26:55 -0300

    bla

--- src/settings.js
+++ src/settings.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
 // Static
 const publicPath = path.resolve(__dirname, "./public");
