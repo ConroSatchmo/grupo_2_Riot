50d78a31cda53beb073cb74f26e673a2bdbcdb16
ConroSatchmo <conradonegrin@gmail.com>
2022-03-18 23:16:48 -0300

    con base de datos

--- src/newdata/Data/newData.js
+++ src/newdata/Data/newData.js
@@ -1,19 +1,18 @@
 const fs = require("fs");
 const path = require("path");
-const folderData = path.join(__dirname, "./oldData");
-const productsJSON = fs.readFileSync(folderData + "/products.json", "utf-8");
-
+const productsJSON = fs.readFileSync("./products.json", "utf-8");
 let products = JSON.parse(productsJSON);
-
 products.forEach((product) => {
   console.log(
-    "INSERT INTO products (`id`, `name`, `description`, `price`, `brand_id`, `color_id`) " +
+    "INSERT INTO products (id, name, description, price, brand_id, color_id) " +
       "VALUES(" +
       " '" +
       product.id +
       "'" +
       ", " +
+      " '" +
       product.name +
+      " '" +
       ", " +
       "'" +
       product.description +
@@ -24,11 +23,11 @@ products.forEach((product) => {
       "'" +
       ", " +
       "'" +
-      "default" +
+      1 +
       "'" +
       "," +
       "'" +
-      "default" +
+      1 +
       "'" +
       ")" +
       ";"
